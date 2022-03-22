252b9a7b6f44eaf744f7d85295d26b110fe27f15
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-03-23 00:50:42 +0100

    creando rutas de pruebas

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,16 +1,20 @@
 #En este archivo, vamos a tener todas las rutas de una forma organizada
 #Blueprint -> son los módulos con los que se construye la aplicación, su objetivo es la organización del códio
-from flask import Blueprint
+#render_template -> es un módulo que sirva para distribuit un archivo html al navegador
+from flask import Blueprint, render_template
 
 contacts = Blueprint ('contacts', __name__)
 
 #En las rutas, se pueden devolver tanto un string como un html
 @contacts.route('/')
 def home():
-    return '<h1>contacts list </h1>'
+    return render_template('index.html')
 
 
 @contacts.route('/new')
 def add_contact():
     return 'saving a contact'
 
+@contacts.route('/abaut')
+def abaut():
+    return render_template('abaut.html')
