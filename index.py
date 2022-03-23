99bbf6619b5fb37dc6b86060799a1f1139766a70
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-03-23 01:23:48 +0100

    solucionando errores

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 #Este archivo se va a utilizar para que la aplicación empiece a funcionar
 from app import app
+from utils.db import db
+
+#Con esta función, va a crear las tablas que hemos definido en contact.py
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
