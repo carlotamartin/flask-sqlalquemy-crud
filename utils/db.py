a910a04ff74f3bb7c63fc48a1e02b098a51b75ff
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-03-23 01:18:56 +0100

    cremaos el contacto y la página db

---
+++ utils/db.py
@@ -0,0 +1,3 @@
+#Este archivo lo utilizaremos para enlazar el formulario con nuestra base de datos
+#Importamos flask_sqlalchemy que es la librería que une el formulario (página web) con nuestra base de datos
+from flask_sqlalchemy import SQLAlchemy
